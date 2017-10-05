b1b406d58bbe1eb7e3fdf7a0f6c76baff673234d
Rachel Coster <rachel.coster@outlook.com>
2017-10-05 15:12:50 +1100

    Added in Question Two

---
+++ Question_two_solution_rachel_coster/Question_two_solution_rachel_coster/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_two_solution_rachel_coster
+{
+    class Program {     
+        private static int[] UniqueArray(int a_len)
+        {
+            Random rnd = new Random();
+            var arr = Enumerable.Range(1, a_len).OrderBy(x => rnd.Next()).Take(a_len).ToArray();
+            return arr; 
+        }
+        static void Main(string[] args)
+          
+        {
+            int[] test = UniqueArray(52);
+       
+           Console.WriteLine("{0}", string.Join(" ",test ));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
